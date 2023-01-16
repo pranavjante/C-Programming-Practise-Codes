633afccf3e1ef83a533a147dbeb282a3995f5967
Pranav Jante <121565279+pranavjante@users.noreply.github.com>
2023-01-16 14:34:41 +0530

    Create Use of for loop using functions.c

---
+++ Use of for loop using functions.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+int line(int nline , char pattern)
+{
+    for(int i=0; i<nline; i++)
+    {
+        printf("X");
+    }
+    return nline;
+}
+
+int triangle(int ncolns, int nrows)
+{
+	for(int i=1;i<=ncolns;i++)
+	{
+		for(int j=0; j< i; j++)
+		{
+			printf("*");
+		}	
+		printf("\n");
+	}
+}
+int rectangle(rcolns,rrows)
+{
+	for(int i=0;i<=rrows;i++)
+	{
+		for(int j=1;j<=rrows;j++)
+		{
+			printf("#");
+		}
+	printf("\n");
+	}
+}
+
+	
+int main() 
+{
+    int nline, ncolns, nrows, rcolns, rrows;
+    printf("\nEnter no. of characters:");
+    scanf("%d", &nline);	
+	printf("\nEnter no. of columns:");
+	scanf("%d", &ncolns);
+	printf("\nEnter no. of columns in rectangle:");
+	scanf("%d",&rcolns);
+	printf("\nEnter no. of rows in rectangle:");
+	scanf("%d",&rrows);
+	line(ncolns, 'X');
+	printf("\n");
+	triangle(ncolns,nrows);
+	rectangle(rcolns,rrows);
+    
+    return 0;
+}
