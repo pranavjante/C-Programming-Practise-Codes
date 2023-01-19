1b7211da12b7d1966a2f80675b5cd56bbb1e75cb
Pranav Jante <121565279+pranavjante@users.noreply.github.com>
2023-01-19 12:13:58 +0530

    Create Finding the maximum number in an array.c

---
+++ Finding the maximum number in an array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int ages[10];
+    int agemax=0;
+    for(int i=0;i<10;i++)
+    {
+        scanf("%d",&ages[i]);
+        if(agemax>ages[i])
+        {
+            agemax=ages[i];
+        }
+    }
+    printf("The maximum age is %d",agemax);
+    return 0;
+}
